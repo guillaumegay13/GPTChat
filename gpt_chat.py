bb07d60e6d346956517def0037db5449910453fb
guillaume.gay@protonmail.com <guillaume.gay@protonmail.com>
2023-11-07 08:42:45 +0100

    Adaptation to gpt-4-1106-preview model

--- gpt_chat.py
+++ gpt_chat.py
@@ -1,9 +1,5 @@
-import os
 import openai
-from transformers import GPT2TokenizerFast
-
-# Set the tokenizers parallelism environment variable to false
-os.environ["TOKENIZERS_PARALLELISM"] = "false"
+import time
 
 class GPTChat:
     """
@@ -26,71 +22,53 @@ class GPTChat:
             api_key (str): The OpenAI API key to use for authentication.
         """
         openai.api_key = api_key
-        self.completion_model = "gpt-4"
+        self.completion_model = "gpt-4-1106-preview"
         self.chat_history = []
-        self.tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
-        self.tokenizer.model_max_length = 8192  # Set the tokenizer maximum length to match GPT-4's limit
+        self.tokenizer.model_max_length = 128000 # 128K context window
 
-    def send_message(self, message, systemContent, temperature, top_p, max_history=5):
+    def send_message(self, message, systemContent, temperature, top_p):
         """
-        Sends a message to the GPT-4 chat model and returns the generated response.
-
-        Args:
-            message (str): The message to send to the GPT-4 chat model.
-            systemContent (str): System-level content that provides context for the model.
-            temperature (float): Sampling temperature to use for generating responses.
-            top_p (float): Nucleus sampling top p value for generating responses.
-            max_history (int): The maximum number of message-response pairs to include in the context.
-
-        Returns:
-            str: The generated response from the GPT-4 chat model.
+        Sends a message to the GPT-4 chat model, using the maximum tokens available,
+        while keeping the entire latest response in memory. Also prints the time taken
+        for the API call to complete.
         """
 
-        # Prepare the messages in the format expected by the ChatCompletion API
+        # Prepare the system content
         messages_to_send = [
-            {"role": "system", "content": systemContent},
-            {"role": "user", "content": message}
+            {"role": "system", "content": systemContent}
         ]
 
-        # Add the history to the messages to send, maintaining the order
-        history = self.chat_history[-max_history:]
-        for prompt, response in history:
-            messages_to_send.extend([
-                {"role": "user", "content": prompt},
-                {"role": "assistant", "content": response}
-            ])
+        # Include the entire history in the context if it exists
+        if self.chat_history:
+            messages_to_send.append(self.chat_history)
+        
+        # Add the new message to the context
+        messages_to_send.append({"role": "user", "content": message})
 
-        # Calculate the number of tokens used by the messages
-        message_token_lengths = [len(self.tokenizer.encode(m['content'])) for m in messages_to_send]
-        total_message_tokens = sum(message_token_lengths)
-
-        # Calculate the remaining tokens for the model's response
-        remaining_tokens = 8192 - total_message_tokens - 100
-
-        # Ensure that the response does not exceed the remaining token count
-        max_response_tokens = remaining_tokens
+        # Start timing the API call
+        start_time = time.time()
 
         # Send the message to the API
         try:
             response = openai.ChatCompletion.create(
-                model=self.completion_model,
-                temperature=temperature,
-                top_p=top_p,
-                messages=messages_to_send,
-                max_tokens=max_response_tokens
+                model = self.completion_model,
+                temperature = temperature,
+                top_p = top_p,
+                messages = messages_to_send,
+                max_tokens = 4096 # max response token for GPT-4 turbo
             )
         except openai.error.InvalidRequestError as e:
-            print(f"An error occurred: {e}")
-            return None
+            raise e  # Raising the error to be handled by the caller
+
+        # Calculate the duration of the API call
+        duration = time.time() - start_time
+        print(f"API call duration: {duration:.2f} seconds")
 
         # Extract the response content
         message_response = response.choices[0].message['content'].strip()
 
-        # Update the chat history
-        self.chat_history.append((message, message_response))
+        # Update the chat history with the new prompt and response
+        self.chat_history.append({"role": "user", "content": message}, {"role": "system", "content": message_response})
 
-        # Truncate the chat history if necessary
-        if len(self.chat_history) > max_history * 2:
-            self.chat_history = self.chat_history[-max_history * 2:]
-
+        # Return the new response
         return message_response
